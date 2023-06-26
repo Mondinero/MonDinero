9be56bd4799274d90fd8db99a30ff6e6f831a1d1
kneazle714 <kristenyueran@gmail.com>
2023-06-26 15:33:56 -0400

    server set up

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+}
+
+app.use('/assets', express.static(path.resolve(__dirname, '../src/assets')));
+
+// Router to be updated
+// app.use('/server', myRouter);
+
+// 404 error handler
+app.use((req, res) => {
+  return res.status(404).send("This is not the page you're looking for");
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.status(500).send({ error: err });
+});
+
+// Starting server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
