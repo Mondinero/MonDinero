f793f72a214a417ac5bbfb76b72bbcc4bb2d3653
Kevin Fan <kevinlifan@yahoo.com>
2023-06-27 10:50:48 -0400

    saved userID to res.locals

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,6 +8,7 @@ const userController = {
     try {
       const data = await db.query(sqlQuery, [username]);
       if (!data.rows[0]) return next('incorrect username or password');
+      res.locals._id = data.rows[0]._id;
       const compare = await bcrypt.compare(password, data.rows[0].password);
       if (!compare) return next('incorrect username or password');
       else return next();
@@ -19,9 +20,10 @@ const userController = {
   createUser: async (req, res, next) => {
     const { firstName, lastName, username, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
-    const sqlQuery = 'INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)';
+    const sqlQuery = 'INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4) RETURNING _id';
     try {
-      await db.query(sqlQuery, [firstName, lastName, username, hashPassword]);
+      const data = await db.query(sqlQuery, [firstName, lastName, username, hashPassword]);
+      res.locals._id = data.rows[0]._id
       return next();
     } catch (err) {
       return next(err);
