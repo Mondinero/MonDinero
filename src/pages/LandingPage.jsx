bba9f4f3e1e13e6cb8569319821e37bb6ccd7e46
Kevin Fan <kevinlifan@yahoo.com>
2023-06-27 16:23:21 -0400

    merged

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,7 +12,6 @@ export default function LandingPage() {
       document.body.style.setProperty('--scroll', window.scrollY);
       let bodyScroll = document.querySelector('body').style.cssText.slice(10);
       bodyScroll = Number(bodyScroll.slice(0, bodyScroll.length - 1));
-
       if (bodyScroll < 2400) document.querySelector('body').setAttribute('position', 'top');
       else if (bodyScroll > 3900 && bodyScroll < 6300)
         document.querySelector('body').setAttribute('position', 'middle');
@@ -22,16 +21,6 @@ export default function LandingPage() {
       else document.querySelector('body').setAttribute('position', '');
 
       if (bodyScroll > 7600) document.querySelector('body').setAttribute('arrow', 'stop');
-      if (bodyScroll < 2000)
-        document.querySelector('body').setAttribute('position', 'top');
-      else if (bodyScroll > 5000 && bodyScroll < 7000)
-        document.querySelector('body').setAttribute('position', 'middle');
-      else if (bodyScroll > 9000)
-        document.querySelector('body').setAttribute('position', 'bottom');
-      else document.querySelector('body').setAttribute('position', '');
-
-      if (bodyScroll > 7000)
-        document.querySelector('body').setAttribute('arrow', 'stop');
       else document.querySelector('body').setAttribute('arrow', '');
 
       if (bodyScroll > 10000) document.querySelector('body').setAttribute('zoom', 'zoom');
@@ -46,21 +35,18 @@ export default function LandingPage() {
   let selectedDay;
   let selectedNight;
 
-  if (
-    window.matchMedia('(prefers-color-scheme: dark)').matches &&
-    colorTheme === null
-  ) {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches && colorTheme === null) {
     dispatch(setColorTheme('dark'));
   }
 
   document.querySelector('body').setAttribute('theme', colorTheme);
 
   if (colorTheme === 'dark') {
-    currTheme = <i className="fa-solid fa-moon"></i>;
-    selectedNight = <i className="fa-solid fa-check"></i>;
+    currTheme = <i className='fa-solid fa-moon'></i>;
+    selectedNight = <i className='fa-solid fa-check'></i>;
   } else {
-    currTheme = <i className="fa-solid fa-sun"></i>;
-    selectedDay = <i className="fa-solid fa-check"></i>;
+    currTheme = <i className='fa-solid fa-sun'></i>;
+    selectedDay = <i className='fa-solid fa-check'></i>;
   }
 
   return (
@@ -91,45 +77,25 @@ export default function LandingPage() {
         <br />
         <i className={`fa-solid fa-arrow-up ${styles.upArrow}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn1}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn1}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn2}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn2}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn3}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn3}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn4}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn4}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn5}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn5}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn6}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn6}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn7}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn7}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn8}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn8}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn9}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn9}`}></i>
         <br />
-        <i
-          className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn10}`}
-        ></i>
+        <i className={`fa-solid fa-arrow-up ${styles.upArrow} ${styles.turn10}`}></i>
         <br />
         <p className={styles.welcome}>WELCOME TO MONDINERO</p>
         <p className={styles.motto}>TAKE BACK CONTROL OF YOUR MONEY</p>
@@ -144,8 +110,7 @@ export default function LandingPage() {
         onClick={() => {
           if (colorTheme === 'dark') dispatch(setColorTheme('light'));
           else dispatch(setColorTheme('dark'));
-        }}
-      >
+        }}>
         <i className={`fa-solid fa-moon ${styles.moon}`}></i>
       </button>
       <div className={styles.footer}>
