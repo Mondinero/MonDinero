36b89b9e5a3be5c1f8c6c90b347e220b6dd7e8c1
Kyle Slugg <kyle.slugg@gmail.com>
2023-06-27 12:35:18 -0400

    Reintegrated Redux provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
-import CredentialsContext from './CredentialsProvider';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
