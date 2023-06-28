3cf721e44f1dd92f16d4f5aaf7c131fbcbed616b
Kevin Fan <kevinlifan@yahoo.com>
2023-06-28 10:50:43 -0400

    graph data

--- src/pages/GraphsPage.jsx
+++ src/pages/GraphsPage.jsx
@@ -4,10 +4,56 @@ import styles from './GraphPageStyles.module.scss';
 
 export default function Graphs() {
   const data = {
-    name: 'flare',
+    name: 'June Expenses',
     children: [
-      { name: 'Hello', value: 10 },
-      { name: 'Test', value: 10 },
+      {
+        name: 'Entertainment',
+        children: [
+          { name: 'AMC Movies', value: 14.8 },
+          { name: 'Steam', value: 59.99 },
+        ],
+      },
+      {
+        name: 'Food and Drink',
+        children: [
+          {
+            name: 'Uber Eats',
+            children: [
+              { name: 'June 3', value: 18.92 },
+              { name: 'June 6', value: 13.12 },
+              { name: 'June 13', value: 11.72 },
+              { name: 'June 21', value: 23.65 },
+              { name: 'June 22', value: 17.42 },
+            ],
+          },
+
+          { name: 'McDonalds', value: 89.92 },
+        ],
+      },
+      {
+        name: 'Merchandise',
+        children: [
+          { name: 'Apple', value: 2895.98 },
+          { name: 'Amazon', value: 48.96 },
+          { name: 'Costco', value: 213.87 },
+        ],
+      },
+      {
+        name: 'Transportation',
+        children: [
+          { name: 'Sunoco Gas', value: 45.94 },
+          { name: 'Septa', value: 14 },
+          { name: 'NYC Metro', value: 2.5 },
+        ],
+      },
+      {
+        name: 'Travel',
+        children: [
+          { name: 'Hilton', value: 295.95 },
+          { name: 'American Airlines', value: 495.23 },
+        ],
+      },
+      { name: 'Rent and Utilities', value: 1450 },
     ],
   };
 
