242395cbf00a9c2e87883db0959ceba8a5ed5a18
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-06-28 00:28:01 -0400

    login routes working and main page functionality semi working

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
-userRouter.post('/login', userController.checkUser, (req, res) => res.sendStatus(200));
+userRouter.post('/login', userController.checkUser, (req, res) => res.json(res.locals));
 
-userRouter.post('/signup', userController.createUser, (req, res) => res.sendStatus(200));
+userRouter.post('/signup', userController.createUser, (req, res) => res.json(res.locals));
 
 module.exports = userRouter;
