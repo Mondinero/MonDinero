763354d4d5c745660a718f388c1b1492013bd0bb
Kyle Slugg <kyle.slugg@gmail.com>
2023-06-28 10:21:11 -0400

    Tweaks to controllers

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setLinkToken } from '../store/slices/credentialSlice';
 import { usePlaidLink } from 'react-plaid-link';
 
-const Link = (props) => {
-  const { linkToken } = props;
+const Link = () => {
+  const linkToken = useSelector((state) => state.credentialSlice.linkToken);
+  const dispatch = useDispatch();
+
+  const generateToken = async () => {
+    const resp = await fetch('/api/create_link_token', { method: 'POST' });
+    const data = await resp.json();
+    dispatch(setLinkToken(data.link_token));
+  };
+
+  useEffect(() => {
+    generateToken();
+  }, []);
+
+  useEffect(() => {
+    console.log('Logging linkTokens obj from link component:');
+    console.log(linkToken);
+  }, [linkToken]);
+
   const onSuccess = (public_token, metadata) => {
     console.log('Metadata:');
     console.dir(metadata);
     fetch('/api/exchange_public_token', {
       method: 'POST',
       body: JSON.stringify({ public_token }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }
     })
       .then((resp) => {
         if (resp.ok) {
           console.log('Successfully stored access token');
         } else {
-          console.log('Received response other than OK when storing access token');
+          console.log(
+            'Received response other than OK when storing access token'
+          );
         }
       })
       .catch((err) => {
-        console.log(`Encountered error while requesting to exchange/store access token: ${err}`);
+        console.log(
+          `Encountered error while requesting to exchange/store access token: ${err}`
+        );
       });
   };
 
   const config = {
     token: linkToken,
     receivedRedirectUri: null,
-    onSuccess,
+    onSuccess
   };
   const { open, ready, error } = usePlaidLink(config);
   if (error) {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,17 @@
-import React, { useEffect } from 'react';
-import Link from '../components/link';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setLinkToken } from '../store/slices/credentialSlice';
-import { TransactionsBar } from '../components/vizRepository';
 import styles from '../styles/Home.module.scss';
-
 
 import {
   setFirstName,
   setErrorMsg,
   setMonthlyIncome,
   setExpenses
-} from '../store/slices/appSlice'
+} from '../store/slices/appSlice';
 function HomePage() {
   const dispatch = useDispatch();
   const currentDate = new Date();
   const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
-
 
   const firstName = useSelector((state) => state.appSlice.firstName);
   const totalExpenses = useSelector((state) => state.appSlice.expenses);
@@ -25,108 +20,90 @@ function HomePage() {
   const expensesArr = [];
 
   for (let key in totalExpenses) {
-
     expensesArr.push(
       <div>
-         <span>{key}</span>
-         <span>{totalExpenses[key]}</span>
+        <span>{key}</span>
+        <span>{totalExpenses[key]}</span>
       </div>
-     
     );
   }
 
-
-
   function addExpense(e) {
-    e.preventDefault()
+    e.preventDefault();
     const val = document.getElementById('expense');
     //const value = e.value;
     const desc = val.options[val.selectedIndex].text;
-   
+
     const amount = document.getElementById('amount').value;
 
-    dispatch(setExpenses({[desc]: amount}))
-    
-    
+    dispatch(setExpenses({ [desc]: amount }));
   }
-
-  const linkToken = useSelector((state) => state.credentialSlice.linkToken);
-
-  const generateToken = async () => {
-    const resp = await fetch('/api/create_link_token', { method: 'POST' });
-    const data = await resp.json();
-    dispatch(setLinkToken(data.link_token));
-  };
-
-  // useEffect(() => {
-  //   generateToken();
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log('Logging linkTokens obj from home page:');
-  //   console.log(linkToken);
-  // }, [linkToken]);
 
   return (
     <React.Fragment>
       <div className={styles.container}>
         <div>
-        <span>Welcome </span>
-        <span>{firstName}</span>
+          <span>Welcome </span>
+          <span>{firstName}</span>
         </div>
-        
+
         <div>
           <span>Set up your income and monthly budget for</span>
           <span>{currentMonth}</span>
         </div>
 
-
         <div>
-          <span>Income <input type="number"  min="1" step="any" /></span>
+          <span>
+            Income <input type="number" min="1" step="any" />
+          </span>
           <button
             onClick={() => {
-              dispatch(setMonthlyIncome())
+              dispatch(setMonthlyIncome());
             }}
-          >Add income</button>
+          >
+            Add income
+          </button>
         </div>
 
         <div>
           <span>Total Income</span>
           <span>{useSelector((state) => state.appSlice.monthlyIncome)}</span>
         </div>
-        
+
         <p>Enter Your Expenses</p>
 
         <div>
           {/* <label for="expense">Select an Expense:</label> */}
           <select name="expense" id="expense">
-            <option value="rent" >Rent</option>
+            <option value="rent">Rent</option>
             <option value="food and drink">Food and Drink</option>
             <option value="entertainment">Entertainment</option>
             <option value="transportation">Transportation</option>
           </select>
-          
-            <input type="number" placeholder="Amount" id='amount'  min="1" step="any" />
-            <button onClick={(e) => {
-              addExpense(e)
-            }} type="submit">Add</button>
-        
+
+          <input
+            type="number"
+            placeholder="Amount"
+            id="amount"
+            min="1"
+            step="any"
+          />
+          <button
+            onClick={(e) => {
+              addExpense(e);
+            }}
+            type="submit"
+          >
+            Add
+          </button>
         </div>
 
-        <div>
-          {expensesArr}
-        </div>
+        <div>{expensesArr}</div>
 
         <button>Save budget</button>
-     
       </div>
-
-      <TransactionsBar />
-      <Link linkToken={linkToken} />
     </React.Fragment>
-   
-  )
-
+  );
 }
 
 export default HomePage;
